1defd8adf1cbe6730cbc8150ae22c75a58522704
Emilia-Dumitru <dumitruemilia41@gmail.com>
2024-05-11 14:25:39 +0300

    am terminat ex3

---
+++ ex3-tema2.js
@@ -0,0 +1,15 @@
+//Creați o funcție care inversează un șir de numere
+
+const numbers =[1, -5, 20, -34, 16, 29, 36, -4];
+numbers.reverse();
+console.log (numbers);
+
+const numbers2 =[18,35,178,-9,8];
+numbers2.reverse();
+console.log (numbers2);
+
+//Cum sa aranjam numerele in ordine crescatoare
+numbers.sort();
+console.log(numbers);
+
+

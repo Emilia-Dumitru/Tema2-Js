f84705260a09727d44c8956a80fd21eec652f8c3
Emilia-Dumitru <168010385+Emilia-Dumitru@users.noreply.github.com>
2024-05-13 21:25:31 +0300

    Update ex4-tema2.js
    Co-authored-by: Marcel <buianmarcel@gmail.com>

--- ex4-tema2.js
+++ ex4-tema2.js
@@ -3,7 +3,7 @@
 //vocale găsite într-un șir. Aceasta este o modalitate excelentă de a practica determinarea caracteristicilor unui set de date.
 const str = "Acesta este un string";
 const countVowels = (str) => {
-    str = str.toLowerCase ();
+    str = str.toLowerCase();
     const vowels = "aeiou";
     let count = 0;
     for ( let i=0; i< str.length; i++){

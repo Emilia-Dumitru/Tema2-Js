1593b56859bce9aa033b6714ea81d4b20facc164
Emilia-Dumitru <168010385+Emilia-Dumitru@users.noreply.github.com>
2024-05-13 21:25:25 +0300

    Update ex4-tema2.js
    Co-authored-by: Marcel <buianmarcel@gmail.com>

--- ex4-tema2.js
+++ ex4-tema2.js
@@ -2,7 +2,7 @@
 //Creați o funcție care va returna un număr întreg al numărului de 
 //vocale găsite într-un șir. Aceasta este o modalitate excelentă de a practica determinarea caracteristicilor unui set de date.
 const str = "Acesta este un string";
-const countVowels = (str = "") => {
+const countVowels = (str) => {
     str = str.toLowerCase ();
     const vowels = "aeiou";
     let count = 0;
